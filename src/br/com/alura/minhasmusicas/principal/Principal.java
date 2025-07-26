f3edffad659b05155155b09a10aaf9c27231e1a5
DeyvissonRobert <robert.designerx@gmail.com>
2025-07-26 09:15:11 -0300

    Desafio de musicas da alura feito

--- src/br/com/alura/minhasmusicas/principal/Principal.java
+++ src/br/com/alura/minhasmusicas/principal/Principal.java
@@ -1,4 +1,37 @@
 package br.com.alura.minhasmusicas.principal;
 
+import br.com.alura.minhasmusicas.modelos.MinhasPreferidas;
+import br.com.alura.minhasmusicas.modelos.Musica;
+import br.com.alura.minhasmusicas.modelos.Podcast;
+
 public class Principal {
+    public static void main(String[] args) {
+        Musica minhaMusica = new Musica();
+        minhaMusica.setTitulo("NÃO PARE");
+        minhaMusica.setCantor("Teto");
+        for (int i = 0; i < 1000; i++) {
+            minhaMusica.reproduz();
+        }
+
+        for (int i = 0; i < 50; i++) {
+            minhaMusica.curte();
+        }
+
+        Podcast meuPodcast = new Podcast();
+        meuPodcast.setTitulo("Sai da Média");
+        meuPodcast.setApresentador("Geronimo Theml & Patricia Araújo");
+
+        for (int i = 0; i < 5000; i++) {
+            meuPodcast.reproduz();
+        }
+
+        for (int i = 0; i < 1000; i++) {
+            meuPodcast.curte();
+        }
+
+        MinhasPreferidas preferidas = new MinhasPreferidas();
+        preferidas.inclui(meuPodcast);
+        preferidas.inclui(minhaMusica);
+
+    }
 }
